ba0a39ff918a36aa3622611cba9e1723fe81c2eb
NhatTruong <truongmai750@gmail.com>
2023-11-06 09:36:23 +0700

    route in react

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import MovieCard from "./components/movie/MovieCard";
 import { SwiperSlide, Swiper } from "swiper/react";
 import "swiper/scss";
-import MovieList from "./components/movie/MovieList";
+import Main from "./components/layout/Main";
+import HomePage from "./pages/HomePage";
 import Banner from "./components/banner/Banner";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-5">
-        <span className="text-primary">Home</span>
-        <span>Movies</span>
-      </header>
-      <Banner></Banner>
-      <section className="movies-layout page-container-fluid pb-20">
-        <h2 className="capitalize text-white mb-5 text-3xl font-bold">
-          Now playing
-        </h2>
-        <MovieList></MovieList>
-      </section>
-      <section className="movies-layout page-container-fluid pb-20">
-        <h2 className="capitalize text-white mb-5 text-3xl font-bold">
-          Top Rated
-        </h2>
-        <MovieList type="top_rated"></MovieList>
-      </section>
-      <section className="movies-layout page-container-fluid pb-20">
-        <h2 className="capitalize text-white mb-5 text-3xl font-bold">
-          Top Trending
-        </h2>
-        <MovieList type="popular"></MovieList>
-      </section>
+      <Routes>
+        <Route element={<Main></Main>}>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }
+          ></Route>
+          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+        </Route>
+      </Routes>
     </Fragment>
   );
 }
---
+++ src/pages/MoviePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MoviePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem quod quo
+      eum illo nisi, nam, reprehenderit itaque, eligendi numquam qui minima
+      voluptates nihil? Deserunt sint assumenda labore consectetur, perspiciatis
+      odio.
+    </div>
+  );
+};
+
+export default MoviePage;
