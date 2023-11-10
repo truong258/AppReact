d4626845b78ad1f6d9450dc931826c9e40d048ec
NhatTruong <truongmai750@gmail.com>
2023-11-10 13:30:36 +0700

    loaing phan trang

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -5,28 +5,39 @@ import { fetcher } from "../config";
 import MovieCard from "../components/movie/MovieCard";
 import useDebounce from "../useDebounce";
 
+const pageCount = 5;
+
 const MoviePage = () => {
+  const [nextPage, setNextPage] = useState(1);
   const [filter, setFilter] = useState("");
   const [url, setUrl] = useState(
-    "https://api.themoviedb.org/3/movie/popular?api_key=a2c6e1101cef616ebfd793b414446804"
+    `https://api.themoviedb.org/3/movie/popular?api_key=a2c6e1101cef616ebfd793b414446804&page=${nextPage}`
   );
-  const filterDebounce = useDebounce(filter, 500);
+  const filterDebounce = useDebounce(filter, 1500);
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-  const { data } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher);
+  const loading = !data && !error;
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
-        `https://api.themoviedb.org/3/search/movie?api_key=a2c6e1101cef616ebfd793b414446804&query=${filterDebounce}`
+        `https://api.themoviedb.org/3/search/movie?api_key=a2c6e1101cef616ebfd793b414446804&query=${filterDebounce}&page=${nextPage}`
       );
     } else {
       setUrl(
-        "https://api.themoviedb.org/3/movie/popular?api_key=a2c6e1101cef616ebfd793b414446804"
+        `https://api.themoviedb.org/3/movie/popular?api_key=a2c6e1101cef616ebfd793b414446804&page=${nextPage}`
       );
     }
-  }, [filterDebounce]);
+  }, [filterDebounce, nextPage]);
   const movies = data?.results || [];
+  console.log(data);
+  if (data !== undefined) {
+    const { page, total_pages } = data;
+    console.log("page", page);
+    console.log("total page", total_pages);
+  }
+
   return (
     <div className="py-10">
       <div className="flex mb-10">
@@ -55,12 +66,67 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
-
+      {loading && (
+        <div
+          className="w-10 h-10 rounded-full border-4 border-primary border-t-transparent
+border-t-4 mx-auto animate-spin"
+        ></div>
+      )}
       <div className="grid grid-cols-4 gap-10">
-        {movies.length > 0 &&
+        {!loading &&
+          movies.length > 0 &&
           movies.map((item) => (
             <MovieCard key={item.id} item={item}></MovieCard>
           ))}
+      </div>
+      <div className="flex items-center justify-center mt-10 gap-x-5">
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            onClick={() => {
+              if (nextPage > 1) {
+                setNextPage(nextPage - 1);
+              }
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </span>
+        {new Array(pageCount).fill(0).map((item, index) => (
+          <span
+            className="cursor-pointer inline-block py-2 px-4 rounded leading-none bg-white text-slate-900"
+            onClick={() => setNextPage(index + 1)}
+          >
+            {index + 1}
+          </span>
+        ))}
+
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            onClick={() => setNextPage(nextPage + 1)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
       </div>
     </div>
   );
